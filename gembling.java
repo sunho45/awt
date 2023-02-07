3903e76efff214095665cd7b3b2d4d46aa79591d
sunho45 <100903674+sunho45@users.noreply.github.com>
2023-02-07 11:43:33 +0900

    Update gembling.java

--- gembling.java
+++ gembling.java
@@ -1,74 +1,92 @@
-import javax.swing.*;
+import java.util.*;
+import java.math.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.JLabel;
 import java.awt.*;
 import java.awt.event.*;
 public class QUEUE extends JFrame {
-	private final int FLYING_UNIT=10;
-	
-	private JLabel btn=new JLabel("DEW");
-	private int a=0;
-	private int b=0;
-	private int c=0;
-	JTextField t=new JTextField(Integer.toString(a)); 
-	JTextField tl=new JTextField(Integer.toString(b)); 
-	JTextField tll=new JTextField(Integer.toString(c)); 
-	
-	
-	public QUEUE() {
-		setTitle("My color Test");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	JButton btn=new JButton("겜블링");
+	JTextField a=new JTextField("0");
+	JTextField b=new JTextField("0");
+	JTextField d=new JTextField("0");
+	JLabel j=new JLabel("겜블링 스타트 버튼을 클릭해주세요");
+	Font font = new Font("궁서", Font.BOLD + Font.ITALIC, 25);
+	public QUEUE(){
+		setTitle("gembling");
 		setVisible(true);
-		setSize(350,350);
-		Container c=getContentPane();
-		t.setSize(30, 50);
-		t.setBackground(Color.pink);
-		tl.setSize(30, 50);
-		tll.setSize(30, 50);
-		t.setLocation(50, 10);
-		tl.setLocation(150, 10);
-		tll.setLocation(250, 10);
-		c.add(t);
-		c.add(tl);
-		c.add(tll);
-		
-		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(660,330);
+		btn.setSize(100,50);
+		btn.setLocation(280, 200);
+		btn.addActionListener(new myActionListener());
+		Container c= getContentPane();
 		c.setLayout(null);
-		
-		c.addKeyListener( new MyKeyListener());
-		c.setFocusable(true);
-		c.requestFocus();
-	
-	
+		c.add(btn);
+		c.add(a);
+		a.setSize(80, 100);
+		a.setBackground(Color.pink);
+		a.setForeground(Color.yellow);
+		a.setHorizontalAlignment(SwingConstants.CENTER);
+		a.setFont(font);
+		a.setLocation(100, 50);
+		c.add(b);
+		b.setSize(80, 100);
+		b.setLocation(300, 50);
+		b.setForeground(Color.yellow);
+		b.setBackground(Color.pink);
+		b.setHorizontalAlignment(SwingConstants.CENTER);
+		b.setFont(font);
+		c.add(d);
+		d.setSize(80, 100);
+		d.setLocation(500, 50);
+		d.setForeground(Color.yellow);
+		d.setHorizontalAlignment(SwingConstants.CENTER);
+		d.setBackground(Color.pink);
+		d.setFont(font);
+		c.add(j);
+		j.setSize(200, 100);
+		j.setLocation(400, 150);
 	}
 	
-	class MyKeyListener extends KeyAdapter{
-		public void keyPressed(KeyEvent e) {
-			int KeyCode= e.getKeyCode();
-			if(KeyCode==KeyEvent.VK_ENTER) {
-				System.out.println(3);
-				 t.setText(Integer.toString((int)(Math.random()*5)));
-				 tl.setText(Integer.toString((int)(Math.random()*5)));
-				 tll.setText(Integer.toString((int)(Math.random()*5)));
+class myActionListener implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton te= (JButton)e.getSource();
+		if(te.getText().equals("겜블링")) {
+			int t=(int)(Math.random()*8);
+			int tl=(int)(Math.random()*8);
+			int tll=(int)(Math.random()*8);
+			a.setText(Integer.toString(t));
+			
+			d.setText(Integer.toString(tll));
 			
 			
-			
+			b.setText(Integer.toString(tl));
+			btn.setText("겜블링");
+			if(t==tll&&t==tl) {
+				j.setText("당신은 운이 좋군요");
 			}
-		
-		
+			else {
+				j.setText("야레야레");
+			}
+			
+			
 		}
 		
+	}
 	
-			
-			
-			
-			}
-			
+}
 			
 			
 	
 public static void main(String[] args) {
+	
+	new QUEUE();
 		
-		
-		new QUEUE();
 	}
 	
 }
